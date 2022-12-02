4e42285efa3f52f3601aada389ec690b194e7ebd
mSaLee <ts.zstupid@gmail.com>
2022-12-02 14:29:29 +0700

    Update run.sh
    Signed-off-by: mSaLee <ts.zstupid@gmail.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 rm telegram_gcloner/config.ini
 curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
 curl "https://clonebot.tk/0:/Scalingo%20[R1.59.1]/gclone" >> telegram_gcloner/gclone
+chmod 777 telegram_gcloner/gclone
 npm install http-server -g
 http-server -p 8080 &
 python3 telegram_gcloner/telegram_gcloner.py
